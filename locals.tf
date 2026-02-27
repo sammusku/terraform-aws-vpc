a2a86c5bef99dbb376698242c2a17d6228c3377f
sammusku <Samyuktha.musku@gmail.com>
2026-02-27 13:07:18 -0600

    vpc

--- locals.tf
+++ locals.tf
@@ -11,6 +11,12 @@ locals {
             Name = "${var.project}-${var.environment}"
         },
         var.vpc_tags)
-    }
-
     
+
+     igw_final_tags = merge (
+        local.common_tags,
+        {
+            Name = "${var.project}-${var.environment}"
+        },
+        var.igw_tags)
+    }
