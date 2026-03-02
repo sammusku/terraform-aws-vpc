a0aa8891166f923af3464a41d914eb38365829ba
sammusku <Samyuktha.musku@gmail.com>
2026-03-02 13:31:15 -0600

    vpc

--- variables.tf
+++ variables.tf
@@ -20,7 +20,7 @@ variable igw_tags {
     type = map
     default = {}
 }
-
+######variables of subnets##
 variable public_subnet_cidr {
    type = list 
    default = ["10.0.1.0/24", "10.0.2.0/24"]
@@ -29,4 +29,42 @@ variable public_subnet_cidr {
 variable "public_subnet_tags" {
     default = {}
     type = map
+}
+variable private_subnet_cidr {
+    type = list
+    default = ["10.0.11.0/24","10.0.12.0/24"]
+}
+variable private_subnet_tags {
+    type = map
+    default = {}
+}
+variable database_subnet_cidr {
+    type = list
+    default = ["10.0.21.0/24","10.0.22.0/24"]
+}
+variable database_subnet_tags {
+    type = map
+    default = {}
+}
+
+####### variables of route_tables###
+variable public_route_table_tags {
+    type = map
+    default = {}
+}
+variable private_route_table_tags {
+    type = map
+    default = {}
+}
+variable database_route_table_tags {
+    type = map
+    default = {}
+}
+variable eip_tags {
+    type = map
+    default = {}
+}
+variable nat_gateway_tags {
+   type = map
+   default = {}
 }
