51a0868a09a1dd8326454e40c608e3ea44d23e1c
sammusku <Samyuktha.musku@gmail.com>
2026-04-20 11:50:30 -0500

    clean terraform repo

---
+++ locals.tf
@@ -0,0 +1,24 @@
+locals {
+    common_tags = {
+        project = var.project
+        environment = var.environment
+        terraform = true
+    }
+
+    vpc_final_tags = merge (
+        local.common_tags,
+        {
+            Name = "${var.project}-${var.environment}"
+        },
+        var.vpc_tags)
+    
+
+    igw_final_tags = merge (
+        local.common_tags,
+        {
+            Name = "${var.project}-${var.environment}"
+        },
+        var.igw_tags)
+     #slice(list, start_index, end_index)
+    az_names = slice(data.aws_availability_zones.available.names,0,2)  
+}
---
+++ variables.tf
@@ -0,0 +1,84 @@
+variable project {
+    type = string
+}
+
+variable environment {
+    type = string
+    validation {
+        condition     = contains(["dev", "qa", "uat", "prod"], var.environment)
+        error_message = "Environments should be one of dev, qa, uat or prod"
+    }
+}
+
+
+variable vpc_cidr {
+    type = string
+    default = "10.0.0.0/16"
+}
+
+variable vpc_tags {
+    type = map
+    default = {}
+}
+
+variable igw_tags {
+    type = map
+    default = {}
+}
+######variables of subnets##
+variable public_subnet_cidr {
+   type = list 
+   default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "public_subnet_tags" {
+    default = {}
+    type = map
+}
+variable private_subnet_cidr {
+    type = list
+    default = ["10.0.11.0/24","10.0.12.0/24"]
+}
+variable private_subnet_tags {
+    type = map
+    default = {}
+}
+variable database_subnet_cidr {
+    type = list
+    default = ["10.0.21.0/24","10.0.22.0/24"]
+}
+variable database_subnet_tags {
+    type = map
+    default = {}
+}
+
+####### variables of route_tables###
+variable public_route_table_tags {
+    type = map
+    default = {}
+}
+variable private_route_table_tags {
+    type = map
+    default = {}
+}
+variable database_route_table_tags {
+    type = map
+    default = {}
+}
+variable eip_tags {
+    type = map
+    default = {}
+}
+variable nat_gateway_tags {
+   type = map
+   default = {}
+}
+##### peering connection ##
+variable is_peering_required {
+    type = bool
+    default = false
+} 
+variable peering_tags {
+    type = map
+    default = {}
+}
