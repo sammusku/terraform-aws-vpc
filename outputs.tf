f527ca86a70c1cb8f0bc6456b62884a362fb3032
sammusku <Samyuktha.musku@gmail.com>
2026-04-20 12:03:03 -0500

    terraform-vpc

--- outputs.tf
+++ outputs.tf
@@ -14,6 +14,6 @@ output "private_subnet_ids" {
 output "database_subnet_ids" {
     value = aws_subnet.database[*].id
 }
-output "aws_db_subnet_group_ids" {
-    value = aws_db_subnet_group.roboshop[*].id
+output "database_subnet_group_name" {
+    value = aws_db_subnet_group.roboshop.name
 }
